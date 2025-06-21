5d2ce13de5c0541727ed5745333f08192eaa2717
Kuldeep Pradhan <150110469+Kuldeep-00@users.noreply.github.com>
2025-06-21 22:25:30 +0530

    Cart -Added,authorization,req,res,controller,entity,dto,service

---
+++ src/main/java/com/tastytown/backend/mapper/CartMapper.java
@@ -0,0 +1,23 @@
+package com.tastytown.backend.mapper;
+
+import java.util.List;
+
+import com.tastytown.backend.dto.CartItemResponse;
+import com.tastytown.backend.dto.CartResponseDTO;
+import com.tastytown.backend.entity.Cart;
+
+public class CartMapper {
+    private CartMapper() {}
+
+        public static CartResponseDTO convertToCartResponseDTO(Cart cart) {
+            List<CartItemResponse> items = cart.getItems().stream()
+                        .map(item -> new CartItemResponse(item.getFood().getFoodId(),
+                        item.getQuantity(),
+                        item.getFood().getFoodName(),
+                        item.getFood().getCategory().getCategoryName(),
+                        item.getFood().getFoodPrice())).toList();
+
+            return new CartResponseDTO(items);
+    }
+    
+}
